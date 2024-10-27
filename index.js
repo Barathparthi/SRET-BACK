b9e201d391a4541b9dde5e3a57b462baa5ec8cad
Barath Kumar P <111954856+Barathparthi@users.noreply.github.com>
2024-10-27 14:52:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: ["https://sretportal.netlify.app"],
+    origin: ["http://localhost:3000"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }));
